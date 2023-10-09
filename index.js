52f683f2e232a363e4516cbf2c5aa8fe50cef187
Iker Macaya Faber <iker@macaya.dev>
2023-10-09 10:05:53 +0200

    Installing nodemon

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+let quotes = [
+    {
+        "id": 1,
+        "quote": "Knowledge is Power",
+        "author": "Francis Bacon",
+        "visual": "https://cdn.midjourney.com/6cf63138-02f2-4b6f-b682-38ab2e52dac0/0_2.webp",
+        "prompt": "knowledge is power, in minimalist style"
+    },
+    {
+        "id": 2,
+        "quote": "Unax talking with the moon",
+        "author": "Francis Bacon",
+        "visual": "https://cdn.midjourney.com/69bd4e55-6fa9-48e6-81d9-cc1525feada5/0_0.webp",
+        "prompt": "a two years blonde kid, with short hair, talking with the moon"
+    },
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Visual Quotes API</h1>')
+})
+
+app.get('/api/quotes', (resquest, response) => {
+    response.json(quotes)
+})
+
+const PORT = 8001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
