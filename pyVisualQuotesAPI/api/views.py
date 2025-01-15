757be077050319efe2671905100f12bbc84d636e
Maciker <ikermacaya@gmail.com>
2025-01-15 23:21:35 +0100

    Filter added to views

--- pyVisualQuotesAPI/api/views.py
+++ pyVisualQuotesAPI/api/views.py
@@ -1,11 +1,14 @@
 from rest_framework import generics
 from .models import Quote
 from .serializers import QuoteSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class QuoteListCreateView(generics.ListCreateAPIView):
     queryset = Quote.objects.all()
     serializer_class = QuoteSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['side', 'author']
 
 
 class QuoteRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
