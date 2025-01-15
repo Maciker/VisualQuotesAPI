3a71737aee076406c39eadc968f94d4713b4d603
Maciker <iker@macaya.dev>
2025-01-15 22:36:22 +0100

    load quotes command added

---
+++ pyVisualQuotesAPI/api/management/commands/load_quotes.py
@@ -0,0 +1,18 @@
+import json
+from django.core.management.base import BaseCommand
+from api.models import Quote
+
+class Command(BaseCommand):
+    help = 'Load quotes from a JSON file into the database'
+
+    def handle(self, *args, **kwargs):
+        with open('quotes.json', 'r') as file:
+            quotes = json.load(file)
+            for quote_data in quotes:
+                Quote.objects.create(
+                    id=quote_data['id'],
+                    quote=quote_data['quote'],
+                    author=quote_data['author'],
+                    side=quote_data['side']
+                )
+            self.stdout.write(self.style.SUCCESS('Successfully loaded quotes into the database'))
